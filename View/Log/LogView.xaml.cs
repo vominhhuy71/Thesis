0608e80438348e6def49348418da6570a8fcc10e
Huy Vo <huy.vminh71@gmail.com>
2022-04-24 14:07:57 +0300

    first commit

---
+++ View/Log/LogView.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InventoryManagement.View
+{
+    /// <summary>
+    /// Interaction logic for LogView.xaml
+    /// </summary>
+    public partial class LogView : UserControl
+    {
+        private GridViewColumnHeader listViewSortCol = null;
+        public LogView()
+        {
+            InitializeComponent();
+            ItemList.Items.SortDescriptions.Add(new SortDescription("Date", ListSortDirection.Descending));
+        }
+
+        private void ListView_SizeChanged( object sender, SizeChangedEventArgs e )
+        {
+            ListView listView = sender as ListView;
+            GridView gridView = listView.View as GridView;
+
+            var width = listView.ActualWidth - SystemParameters.VerticalScrollBarWidth;
+
+            var column1 = 0.30;
+            var column2 = 0.10;
+            var column3 = 0.10;
+            var column4 = 0.20;
+            var column5 = 0.30;
+
+
+            gridView.Columns[0].Width = width * column1;
+            gridView.Columns[1].Width = width * column2;
+            gridView.Columns[2].Width = width * column3;
+            gridView.Columns[3].Width = width * column4;
+            gridView.Columns[4].Width = width * column5;
+
+        }
+
+        private void GridViewColumnHeader_Click( object sender, RoutedEventArgs e )
+        {
+            GridViewColumnHeader column = (e.OriginalSource as GridViewColumnHeader);
+            string sortBy = column.Tag.ToString();
+            if (listViewSortCol != null)
+            {
+                ItemList.Items.SortDescriptions.Clear();
+            }
+
+            ListSortDirection newDir = ListSortDirection.Ascending;
+            if (listViewSortCol == column)
+            {
+                newDir = ListSortDirection.Descending;
+            }
+
+
+            listViewSortCol = column;
+            ItemList.Items.SortDescriptions.Add(new SortDescription(sortBy, newDir));
+        }
+
+
+    }
+}
---
+++ ViewModel/Log/LogsViewModel.cs
@@ -0,0 +1,91 @@
+﻿using InventoryManagement.Model;
+using InventoryManagement.View;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace InventoryManagement.ViewModel
+{
+    public class LogsViewModel : ViewModelBase
+    {
+        #region Fields
+        private ObservableCollection<Log> _logsList;
+        public ObservableCollection<Log> LogsList
+        {
+            get { return _logsList; }
+            set
+            {
+                _logsList = value;
+                OnPropertyChanged("LogsList");
+            }
+        }
+
+        public event Action OnLastReload = delegate { };
+
+        private string _filter { get; set; }
+        public string Filter
+        {
+            get
+            {
+                return _filter;
+            }
+            set
+            {
+                _filter = value;
+                Filterlogs();
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public LogsViewModel()
+        {
+            LogsList = new ObservableCollection<Log>();
+            Load_logs();
+
+            Global.InitializeDispatchTimer(DispatchTimer_LoadData);
+
+        }
+        #endregion
+
+        #region Function
+
+        private void Filterlogs()
+        {
+            if (string.IsNullOrEmpty(Filter))
+            {
+                LogsList = Global.Logs;
+            }
+            else
+            {
+                ObservableCollection<Log> foundlogs = new ObservableCollection<Log>();
+                foreach (var log in Global.Logs)
+                {
+                    if (log.Name.Contains(Filter)) foundlogs.Add(log);
+                }
+                LogsList = foundlogs;
+            }
+        }
+
+        private async void Load_logs()
+        {
+            await Global.Load_Logs();
+            LogsList = Global.Logs;
+
+            Action action = OnLastReload;
+            if (action != null)
+            {
+                action();
+            }
+        }
+
+
+
+        public void DispatchTimer_LoadData( object sender, EventArgs e )
+        {
+            Load_logs();
+        }
+        #endregion
+
+    }
+}
